6f4fdf667bd7194ba5fa93ead8d8eb1a2a5ea042
Johnson Cheung <johnson@shred.app>
2021-06-09 12:31:19 -0700

    updated formatting for list

--- src/punfile.rs
+++ src/punfile.rs
@@ -118,7 +118,7 @@ pub fn print_pun_deps(punfile: &PunFile) {
     let frameworks = &punfile.frameworks;
 
     for framework in frameworks {
-        println!("group: {}, artifact: {}, version: {}", 
+        println!("Group: {}, Artifact: {}, Version: {}", 
             framework.repo_name, 
             framework.name, 
             framework.version);
